f236ad60364f96ec9452a7db4a44f6dc2865d24d
juancgalueweb <juancgalue@icloud.com>
2021-09-22 16:22:05 -0300

    feat: :tada: Initial commit

---
+++ src/components/Components.styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Apps = styled.div`
+  width: 100%;
+  background-color: #ddd;
+  margin: 0 auto;
+  padding: 15px;
+`;
+
+export const Header = styled.div`
+  min-height: 150px;
+  width: 100%;
+  margin-bottom: 15px;
+  background-color: #6aa84f;
+  border: 2px solid #000;
+`;
+
+export const Navigation = styled.div`
+  min-height: 300px;
+  width: 34%;
+  display: inline-block;
+  vertical-align: top;
+  margin-right: 15px;
+  background-color: #6fa8dc;
+  border: 2px solid #000;
+`;
+
+export const Main = styled.div`
+  min-height: 370px;
+  width: 65.2%;
+  display: inline-block;
+  background-color: #e06666;
+  border: 2px solid #000;
+`;
+
+export const SubContent = styled.div`
+  min-height: 200px;
+  width: 31.7%;
+  margin: 10px;
+  display: inline-block;
+  background-color: #ffd966;
+  border: 2px solid #000;
+`;
+
+export const Advertisement = styled.div`
+  min-height: 120px;
+  width: 98%;
+  margin: 10px auto;
+  background-color: #b4a7d6;
+  border: 2px solid #000;
+`;
